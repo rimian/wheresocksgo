212e8969231ae9b5df8e5d7ebcd82b62c40728ea
Rimian Perkins <hello@rimian.com.au>
2016-03-27 17:10:43 +1100

    initital status response (200)

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,2 +1,6 @@
-RSpec.describe ApplicationController do
+RSpec.describe ApplicationController, :type => :controller do
+  it 'responds ok' do
+    get :status
+    expect(response).to have_http_status(:ok)
+  end
 end
