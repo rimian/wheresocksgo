470a8bc6532795fc6b19c5216bff22e4732446ba
Rimian Perkins <hello@rimian.com.au>
2016-03-28 11:08:26 +1100

    version removed from the route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'application#status'
 
   namespace :api do
-    namespace :v1 do
+    scope module: 'v1' do
       get '/', action: :index, controller: :base
     end
   end
