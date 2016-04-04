3d157564f196c4ed6788a35ab9b37625eca78107
Rimian Perkins <hello@rimian.com.au>
2016-04-04 14:03:56 +1000

    initital controller output

--- app/controllers/api/v1/socks_controller.rb
+++ app/controllers/api/v1/socks_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::SocksController < ApplicationController
   def index
     socks = [
-
+      {
+        id: 'de305d54-75b4-431b-adb2-eb6b9e546014',
+        name: 'the sock'
+      }
     ]
 
     render json: socks, status: :ok
