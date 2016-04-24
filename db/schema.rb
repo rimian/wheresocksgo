4b8d5431c67f1f5eaaf78571387e811b655af10c
Rimian Perkins <hello@rimian.com.au>
2016-04-24 18:51:11 +1000

    enable uuid-ossp

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414090904) do
+ActiveRecord::Schema.define(version: 20160424083344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "socks", force: :cascade do |t|
     t.string  "title",     limit: 255
